7d83a43bc2563f07395fdcaa8a94f7d78acd8079
ACTEPUKC <makakakakala@gmail.com>
2024-07-09 20:59:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const serverIds = [
+  process.env.SERVERID1,
+  process.env.SERVERID2,
+  process.env.SERVERID3,
+  process.env.SERVERID4,
+];
+const tokens = [
+  process.env.DISCORD_TOKEN1,
+  process.env.DISCORD_TOKEN2,
+  process.env.DISCORD_TOKEN3,
+  process.env.DISCORD_TOKEN4,
+];
+const clients = [];
+
+for (let i = 0; i < serverIds.length; i++) {
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+  });
+  clients.push(client);
+
+  const token = tokens[i];
+  const serverId = serverIds[i];
+
+  client.on("ready", () => {
+    console.log(`Вошли как ${client.user.tag} на сервере ${serverId}!`);
+    const maxPlayers = "100";
+    setInterval(() => updateActivity(client, serverId, maxPlayers), 30000);
+  });
+
+  client.login(token);
+}
+
+async function updateActivity(client, serverId, maxPlayers) {
+  try {
+    const response = await axios.get(
+      `https://api.battlemetrics.com/servers/${serverId}`
+    );
+    const players = response.data.data.attributes.players;
+    const map = response.data.data.attributes.details.map;
+    const queueTemp = response.data.data.attributes.details.squad_publicQueue;
+    const queue = queueTemp ? `+(${queueTemp})` : "";
+    const activityString = `${players}/${maxPlayers}${queue} ${map}`;
+    client.user.setPresence({ activities: [{ name: activityString }] });
+  } catch (e) {}
+}
