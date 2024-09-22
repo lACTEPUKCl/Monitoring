f871307f7aabdf2c6a2ce2dbd00f688d15ec2233
ACTEPUKC <makakakakala@gmail.com>
2024-09-22 03:57:29 +0300

    add reforger

--- index.js
+++ index.js
@@ -61,7 +61,12 @@ const updateActivity = async (client, serverId, maxPlayers) => {
       `https://api.battlemetrics.com/servers/${serverId}`
     );
     const players = response.data.data.attributes.players;
-    const map = response.data.data.attributes.details.map;
+    let map = response.data.data.attributes.details.map;
+
+    if (!map) {
+      map = response.data.data.attributes.details.reforger.scenarioName;
+    }
+
     const queueTemp = response.data.data.attributes.details.squad_publicQueue;
     const queue = queueTemp ? `+(${queueTemp})` : "";
     const activityString = `${players}/${maxPlayers}${queue} ${map}`;
